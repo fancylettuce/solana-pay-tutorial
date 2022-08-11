82384a3226dbb3538e7725e10fcdd77afa13449b
fancylettuce <eh.career29@gmail.com>
2022-08-11 14:16:47 -0500

    Changed cookies to bananas.

--- lib/products.ts
+++ lib/products.ts
@@ -1,17 +1,17 @@
 export const products = [
   {
-    id: 'box-of-cookies',
-    name: 'Box',
-    description: 'A delicious box of handmade cookies',
-    unitName: 'box', // shows after the price, eg. 0.05 SOL/box
+    id: 'hand-of-bananas',
+    name: 'Hand of Bananas',
+    description: 'A delicious hand of bananas',
+    unitName: 'hand', // shows after the price, eg. 0.05 SOL/hand
     priceSol: 0.05,
     priceUsd: 5,
   },
   {
-    id: 'basket-of-cookies',
-    name: 'Basket',
-    description: 'A large basket of handmade cookies',
-    unitName: 'basket',
+    id: 'bunch-of-bananas',
+    name: 'Bunch of Bananas',
+    description: 'A delicious bunch of bananas',
+    unitName: 'bunch', // shows after the price, eg. 0.05 SOL/hand
     priceSol: 0.1,
     priceUsd: 10,
   }
